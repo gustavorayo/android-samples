907300e333dfdaacadc5ad63211e9d3946625878
gustavorayo <rayogustavo@gmail.com>
2015-03-26 20:18:58 -0600

    create simple list with ArrayAdapter.

---
+++ SimpleList/app/src/main/java/com/gustavorayo/simplelist/MainActivity.java
@@ -0,0 +1,43 @@
+package com.gustavorayo.simplelist;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    String mListElements[] = {"Name 1", "Name 2", "Name 3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListElements);
+
+        //To use our own layout uncomment the following line and comment or delete the above line.
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item, mListElements);
+        ListView mList = (ListView) findViewById(R.id.items);
+        mList.setAdapter(adapter);
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), mListElements[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
